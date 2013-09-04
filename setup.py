d4c0ab6aed1ed084d8067863b10a9c36ff68d3df
otternq <otternq@gmail.com>
2013-09-03 23:46:48 -0700

    incrementing version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 readme = open('README.md').read()
 setup(name='SimpleRender',
-      version='0.1.1',
+      version='0.1.2',
       author='Nick Otter',
       author_email='otternq@gmail.com',
       url='https://github.com/otternq/SimpleRender',
